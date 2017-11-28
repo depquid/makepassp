e071dd18970dcb98bf91a4ff78c2a7c4630bc2a7
josh <josh@localhost.localdomain>
2017-11-28 17:45:25 -0500

    allow use of custom dictionary file

--- makepassp.rb
+++ makepassp.rb
@@ -2,23 +2,27 @@ require 'optparse'
 require 'securerandom'
 
 # Some defaults
-dict_file = '/usr/share/dict/words'
+dict_path = '/usr/share/dict/words'
 max_word_length = 7
 power = 5
 
 OptionParser.new do |opts|
-  opts.banner = "Usage: ruby $0 [options]"
+  opts.banner = "Usage: ruby #{$0} [options]"
 
-  opts.on("-lLENGTH", "--max-word-length LENGTH", Integer, "Maximum length of each word") do |length|
+  opts.on("-dPATH", "--dict-path PATH", String, "Path to dictionary file (default: #{dict_path})") do |path|
+    dict_path = path
+  end
+
+  opts.on("-lLENGTH", "--max-word-length LENGTH", Integer, "Maximum length of each word (default: #{max_word_length})") do |length|
     max_word_length = length
   end
 
-  opts.on("-wCOUNT", "--word-count COUNT", Integer, "Number of words to generate") do |count|
+  opts.on("-wCOUNT", "--word-count COUNT", Integer, "Number of words to generate (default: #{power})") do |count|
     power = count
   end
 end.parse!
 
-words = File.read(dict_file).split /\n/
+words = File.read(dict_path).split /\n/
 
 puts "Dictionary Word Count: \t#{words.count}"
 
